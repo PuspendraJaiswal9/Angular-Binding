57594b700e1cd81c1027ba0f34ff9d37e84e6d2f
puspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2025-03-08 08:47:34 +0530

    [Puspendra]Add. Name checker

--- app.component.ts
+++ app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   title = 'angular_binding';
   imgUrl="../assets/BridgeLabzLogo.png";
   url = "https://www.bridgelabz.com";
-  userName: String ="";
+  userName: string ="";
+  nameError: string ="";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz.";
@@ -18,5 +19,15 @@ export class AppComponent {
   onClick($event: any){
     console.log("Save button is clicked!", $event);
     window.open(this.url,"_blank");
+  }
+
+  onInput($event:any){
+    console.log("Change Event Occured!", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect!";
   }
 }
