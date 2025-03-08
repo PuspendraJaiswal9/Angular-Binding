42a8e7089eb66835da7eb9dacac945ded939bdae
puspendraJaiswal9 <pushpendrajaiswal988@gmail.com>
2025-03-08 08:12:44 +0530

    [puspendra]Add. click logo open new tab

--- app.component.ts
+++ app.component.ts
@@ -8,8 +8,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular_binding';
   imgUrl="../assets/BridgeLabzLogo.png";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick($event: any){
+    console.log("Save button is clicked!", $event);
+    window.open(this.url,"_blank");
   }
 }
